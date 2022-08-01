c350387caf0d8a494d1adea510bdfc069148ba3f
ecunal <ecaliskanunal@gmail.com>
2022-08-01 19:44:36 +0100

    Sprint 1 test cases were created + gitIgnore file was added

---
+++ src/featureElifUnal/Sprint1/S1_TC002.java
@@ -0,0 +1,7 @@
+package featureElifUnal.Sprint1;
+
+public class S1_TC002 {
+    public static void main(String[] args) {
+        System.out.println("Second test case was added to the Sprint1");
+    }
+}
